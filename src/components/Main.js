2a768473592225161356aee20d42d69b043ca9df
waffe_x20 <argueta_eduardo@yahoo.de>
2021-05-13 19:18:17 -0600

    Added JSON files & refactor to components

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Welcome from "./DivWillkommen";
+import Hi from "./DivHotelAuskunft";
+
+const Main = () => {
+  return (
+    <main id="wrapper">
+      <Welcome />
+      <Hi />
+    </main>
+  );
+};
+
+export default Main;
